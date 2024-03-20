0e2dd89d49250df2fe1dbbe305dd18afa326b59d
Vladimir <greathunter@mail.ru>
2024-03-20 22:35:09 +0300

    test

---
+++ lick.py
@@ -0,0 +1,13 @@
+import random
+
+slave = "Вова"
+goddess = "Лилит"
+task = ["делать куни"]
+x = int(random.random() * 69)
+
+i = 0
+while i < x:
+    i += 1
+    print("Лижи письку сучка!!")
+
+print("Ты отлизал мне письку", i, "раз")
